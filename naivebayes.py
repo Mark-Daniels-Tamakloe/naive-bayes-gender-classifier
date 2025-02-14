6bd8e6eab022f5d8b99f40aea1b7a7e08549fbcd
Mark-Daniels Tamakloe <143681165+MarkDaniels1@users.noreply.github.com>
2025-02-13 19:55:50 -0600

    completed 2

--- naivebayes.py
+++ naivebayes.py
@@ -9,44 +9,38 @@ from naivebayesPY import naivebayesPY
 from naivebayesPXY import naivebayesPXY
 
 def naivebayes(x, y, x1):
-# =============================================================================
-#function logratio = naivebayes(x,y,x1);
-#
-#Computation of log P(Y|X=x1) using Bayes Rule
-#Input:
-#x : n input vectors of d dimensions (dxn)
-#y : n labels (-1 or +1)
-#x1: input vector of d dimensions (dx1)
-#
-#Output:
-#logratio: log (P(Y = 1|X=x1)/P(Y=-1|X=x1))
-# =============================================================================
+    # =============================================================================
+    # function logratio = naivebayes(x,y,x1);
+    #
+    # Computation of log P(Y|X=x1) using Bayes Rule
+    # Input:
+    # x : n input vectors of d dimensions (dxn)
+    # y : n labels (-1 or +1)
+    # x1: input vector of d dimensions (dx1)
+    #
+    # Output:
+    # logratio: log (P(Y = 1|X=x1)/P(Y=-1|X=x1))
+    # =============================================================================
 
+    # Convert input matrix x and x1 into NumPy arrays
+    X = np.array(x)
+    X1 = np.array(x1).reshape(-1, 1)  # Ensure column vector
 
-    
-    # Convertng input matrix x and x1 into NumPy matrix
-    # input x and y should be in the form: 'a b c d...; e f g h...; i j k l...'
-    X = np.matrix(x)
-    X1= np.matrix(x1)
-    
-    # Pre-configuring the size of matrix X
-    d,n = X.shape
-    
-# =============================================================================
-# fill in code here
-    # YOUR CODE HERE
-
-    # Compute P(Y=1) and P(Y=-1)
+    # Get prior probabilities
     pos, neg = naivebayesPY(x, y)
 
-    # Compute P(X|Y=1) and P(X|Y=-1)
+    # Get conditional probabilities P(X|Y)
     posprob, negprob = naivebayesPXY(x, y)
 
-    # Compute log P(Y=1|X=x1) / P(Y=-1|X=x1) using Bayes rule
-    log_pos = np.sum(np.log(posprob) * X1 + np.log(1 - posprob) * (1 - X1)) + np.log(pos)
-    log_neg = np.sum(np.log(negprob) * X1 + np.log(1 - negprob) * (1 - X1)) + np.log(neg)
+    # Compute likelihoods P(X1|Y=1) and P(X1|Y=-1)
+    likelihood_pos = np.prod(posprob ** X1 * (1 - posprob) ** (1 - X1), axis=0)
+    likelihood_neg = np.prod(negprob ** X1 * (1 - negprob) ** (1 - X1), axis=0)
 
-    logratio = log_pos - log_neg
+    # Compute posterior probabilities
+    P_Y1_X = likelihood_pos * pos
+    P_Yneg1_X = likelihood_neg * neg
+
+    # Compute log ratio
+    logratio = np.log(P_Y1_X) - np.log(P_Yneg1_X)
 
     return logratio
-# =============================================================================
--- naivebayesCL.py
+++ naivebayesCL.py
@@ -10,41 +10,32 @@ from naivebayesPY import naivebayesPY
 from naivebayesPXY import naivebayesPXY
 
 def naivebayesCL(x, y):
-# =============================================================================
-#function [w,b]=naivebayesCL(x,y);
-#
-#Implementation of a Naive Bayes classifier
-#Input:
-#x : n input vectors of d dimensions (dxn)
-#y : n labels (-1 or +1)
-#
-#Output:
-#w : weight vector
-#b : bias (scalar)
-# =============================================================================
+    # =============================================================================
+    # function [w,b]=naivebayesCL(x,y);
+    #
+    # Implementation of a Naive Bayes classifier
+    # Input:
+    # x : n input vectors of d dimensions (dxn)
+    # y : n labels (-1 or +1)
+    #
+    # Output:
+    # w : weight vector
+    # b : bias (scalar)
+    # =============================================================================
 
+    # Convert input matrix x into NumPy array
+    X = np.array(x)
 
-
-    # Convertng input matrix x and x1 into NumPy matrix
-    # input x and y should be in the form: 'a b c d...; e f g h...; i j k l...'
-    X = np.matrix(x)
-
-    # Pre-configuring the size of matrix X
-    d,n = X.shape
-
-# =============================================================================
-# fill in code here
-    # YOUR CODE HERE
-
-    # Compute class prior probabilities
+    # Compute prior probabilities
     pos, neg = naivebayesPY(x, y)
 
     # Compute conditional probabilities P(X|Y)
     posprob, negprob = naivebayesPXY(x, y)
 
-    # Compute log-odds for linear classification
-    w = np.log(posprob) - np.log(negprob)  # Weight vector
-    b = np.log(pos) - np.log(neg) + np.sum(np.log(1 - negprob) - np.log(1 - posprob))  # Bias term
+    # Compute weights (w)
+    w = np.log(posprob) - np.log(negprob)
 
-    return w,b
-# =============================================================================
+    # Compute bias (b)
+    b = np.log(pos) - np.log(neg)
+
+    return w, b
