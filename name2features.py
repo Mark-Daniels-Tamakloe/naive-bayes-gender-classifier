d2aa5cd46fcb571c077ebfb724b0c06be7b7cab4
Mark-Daniels Tamakloe <143681165+MarkDaniels1@users.noreply.github.com>
2025-02-14 00:23:25 -0600

    completed 19

--- name2features.py
+++ name2features.py
@@ -18,7 +18,7 @@ def name2features(name):
     - TF-IDF normalization for better weight distribution
     """
 
-    d = 1024  # Expanded feature space
+    d = 768  # Expanded feature space
     v = np.zeros(d)
     name = name.lower()
 
